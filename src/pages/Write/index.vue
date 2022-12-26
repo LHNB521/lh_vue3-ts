5a9bbccb04cf43a33cb3c803ec2f338e6e15a662
LHNB521 <1767359165@qq.com>
2022-12-26 18:02:53 +0800

    分类添加

--- src/pages/Write/index.vue
+++ src/pages/Write/index.vue
@@ -6,28 +6,91 @@
     right-toolbar="preview  fullscreen"
     @save="saveHandle"
   />
+  <n-modal
+    v-model:show="showModal"
+    class="custom-card"
+    preset="card"
+    size="huge"
+    :style="bodyStyle"
+    :segmented="segmented"
+    title="分类"
+  >
+    <span>选择或输入添加分类：</span>
+    <n-select v-model:value="selectValue" filterable tag :options="options" />
+    <template #footer>
+      <n-button type="primary" @click="onPositiveClick"> 确认 </n-button>
+    </template>
+  </n-modal>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent } from 'vue';
-interface mdProps {
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useCategoryStore } from '@/store/model/category';
+import { useMessage } from 'naive-ui';
+type mdProps = {
   text: string;
   html: string;
-}
-
-export default defineComponent({
-  setup() {
-    // 点击保存
-    const saveHandle = (md: mdProps, html: mdProps) => {
-      console.log(md);
-    };
-    return {
-      text: '',
-      toolbar: '',
-      saveHandle
-    };
-  }
+};
+type category = {
+  category_name: string;
+  category_description: string;
+};
+const message = useMessage();
+const categoryStore = useCategoryStore();
+const text = ref<string>('');
+const toolbar = ref<string>('');
+const showModal = ref<boolean>(false);
+const bodyStyle = ref<object>({
+  width: '600px'
 });
+const segmented = ref<object>({
+  content: 'soft',
+  footer: 'soft'
+});
+const selectValue = ref<string>('');
+const options = ref<any>([]);
+// 点击保存
+const saveHandle = (md: mdProps, html: mdProps) => {
+  showModal.value = true;
+  console.log(md);
+  options.value.length ? '' : getCategory();
+};
+// 获取分类列表
+const getCategory = async () => {
+  const res = categoryStore.categoryList.length
+    ? categoryStore.categoryList
+    : await categoryStore.getCategory();
+  res.forEach((item: any) => {
+    const { category_name } = item;
+    const option = {
+      label: category_name,
+      value: category_name
+    };
+    options.value.push(option);
+  });
+};
+// 点击确认
+const onPositiveClick = () => {
+  showModal.value = true;
+  const result = options.value.some((item: any) => {
+    if (item.value === selectValue.value) {
+      return true;
+    }
+  });
+  result ? '' : addCategory();
+};
+// 添加分类
+const addCategory = async () => {
+  const req = {
+    category_name: selectValue.value,
+    category_description: `这是${selectValue.value}`
+  };
+  const res = await categoryStore.addCategory(req);
+  message.info(res);
+  showModal.value = false;
+  options.value = [];
+  selectValue.value = '';
+};
 </script>
 
 <style scoped lang="scss">
